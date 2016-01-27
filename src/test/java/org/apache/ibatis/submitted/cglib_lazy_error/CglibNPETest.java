3ff80677a600c6a49f5b80bb747e463f1f076bfb
qiuyesuifeng <cuiqiu.bupt@gmail.com>
2016-01-27 14:49:55 +0800

    *: fix CglibNPETest test bug.

--- src/test/java/org/apache/ibatis/submitted/cglib_lazy_error/CglibNPETest.java
+++ src/test/java/org/apache/ibatis/submitted/cglib_lazy_error/CglibNPETest.java
@@ -37,9 +37,8 @@ public class CglibNPETest {
     Connection conn = null;
 
     try {
-      Class.forName("org.hsqldb.jdbcDriver");
-      conn = DriverManager.getConnection("jdbc:hsqldb:mem:cglib_lazy_eager_no_error", "sa",
-          "");
+      Class.forName("com.mysql.jdbc.Driver");
+      conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:4000/test", "root", "");
 
       Reader reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/cglib_lazy_error/CreateDB.sql");
 
